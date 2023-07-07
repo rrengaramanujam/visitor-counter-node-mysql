cd2d79dd45ea26ac2adb97eaa61a907cae1cd88a
Ramprasad Rengaramanujam <81757137+rrengaramanujam@users.noreply.github.com>
2023-07-07 23:30:49 +0200

    Update db.js
    change to use terraform endpoint

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const { Sequelize } = require('sequelize');
 
-const URI = `${process.env.DB_DIALECT}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+const URI = `${process.env.DB_DIALECT}://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
 
 const db = new Sequelize(URI); 
  
